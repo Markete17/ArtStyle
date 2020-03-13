f51bcda44893af794d92d5c335aa9dc606f0085c
Enrique <kikemarquezjimenez@gmail.com>
2020-03-13 11:32:06 +0100

    Sort Clients
    Precio ascendente y descendente
    Año ascendente y descendente

--- src/main/java/art/main/controller/PaintingController.java
+++ src/main/java/art/main/controller/PaintingController.java
@@ -1,10 +1,15 @@
 package art.main.controller;
 
+import java.util.HashMap;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import art.main.database.Painting;
 import art.main.database.PaintingRepository;
@@ -24,6 +29,25 @@ public class PaintingController {
 	@GetMapping("/")
 	public String loadHome() {
 		return "index";
+	}
+	
+	@RequestMapping("/productSortBy")
+	public String sortBy(Model model,@RequestParam String value) {
+		switch(value) {
+		case "PriceDesc":
+			model.addAttribute("paintings", this.paintingRepository.OrderByPriceDesc());
+			break;
+		case "PriceAsc":
+			model.addAttribute("paintings", this.paintingRepository.OrderByPriceAsc());
+			break;
+		case "YearDesc":
+			model.addAttribute("paintings", this.paintingRepository.OrderByYearDesc());
+			break;
+		case "YearAsc":
+			model.addAttribute("paintings", this.paintingRepository.OrderByYearAsc());
+			break;
+		}
+		return "painting";
 	}
 	
 	@PostMapping("/added_painting")
