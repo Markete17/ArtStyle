7978dc7884b62740413f7906096c2e99789ab89d
Enrique <kikemarquezjimenez@gmail.com>
2020-03-13 10:27:42 +0100

    Updated ArtistRepository

--- src/main/java/art/main/database/ArtistRepository.java
+++ src/main/java/art/main/database/ArtistRepository.java
@@ -1,7 +1,13 @@
 package art.main.database;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ArtistRepository extends JpaRepository<Artist, Long>{
-
+	
+	List<Artist> OrderByName();
+	List<Artist> OrderBySurname();
+	List<Artist> OrderByYear();
+	List<Artist> OrderByCountry();
 }
