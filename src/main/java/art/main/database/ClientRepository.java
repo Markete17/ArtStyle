b92bbd846901a648b8a76ecc85fcbfe7035b6ac4
Enrique <kikemarquezjimenez@gmail.com>
2020-03-13 10:27:55 +0100

    Update ClientRepository

--- src/main/java/art/main/database/ClientRepository.java
+++ src/main/java/art/main/database/ClientRepository.java
@@ -1,7 +1,14 @@
 package art.main.database;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ClientRepository extends JpaRepository<Client, Long> {
-
+	
+	List<Client> OrderByNameAsc();
+	List<Client> OrderByNameDesc();
+	
+	List<Client> OrderBySurnameAsc();
+	List<Client> OrderBySurnameDesc();
 }
